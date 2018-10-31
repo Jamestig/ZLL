bca161c3f4b68ca8331f106e7d63370cc684721f
Jamestig <tim.j.ringuet@gmail.com>
2018-10-31 17:51:37 +0700

    New direction

--- dist/js/center.js
+++ dist/js/center.js
@@ -1,37 +1,16 @@
 $(document).ready(function () {
-  var windowHeight = $(window).height(),
-    gridTop = windowHeight * .00001,
-    gridBottom = windowHeight * .99999;
+	var windowHeight = $(window).height();
+	var windowTop = windowHeight * 0.5;
 
   $(window).on('scroll', function () {
     $('.frame').each(function () {
-      var thisTop = $(this).offset().top - $(window).scrollTop();
+			var thisTop = $(this).offset().top - $(window).scrollTop();
 
-      if (thisTop > gridTop && (thisTop + $(this).height()) < gridBottom) {
-        $(this).css('opacity', 1);
+      if (thisTop > windowTop) {
+				$(this).find('p').css('opacity', 0);
       } else {
-        $(this).css('opacity', .4);
+				$(this).find('p').css('opacity', 1);
       }
     });
-  });
-});
-
-$(window).scroll(function() {
-	
-	$('.frame').each(function() {
-		var topHalf = $(windowHeight / 2);
-		if ( $(this).offset().top > topHalf) {
-			console.log($(this));
-		}
-	});
-});
-
-$(function() {
-  var eTop = $('.frame').offset().top; //get the offset top of the element
-  log(eTop - $(window).scrollTop()); //position of the ele w.r.t window
-
-  $(window).scroll(function() { //when window is scrolled
-		log(eTop - $(window).scrollTop());
-		console.log(eTp)
   });
 });
--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 	$(window).scroll(function () {
 
 		$('.frame').each(function () {
-			var topHalf = $(window).height() / 2;
+			var topHalf = $(window).height() * 0.2;
 			console.log(topHalf);
 			if ($(this).offset().top > topHalf) {
 				console.log($(this));
